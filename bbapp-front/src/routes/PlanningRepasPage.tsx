800d17e4b1721e558ed82bad125667ba3572a98a
ofrancois <olivier.francois09@gmail.com>
2024-05-11 11:01:29 +0200

    test dead area

--- bbapp-front/src/routes/PlanningRepasPage.tsx
+++ bbapp-front/src/routes/PlanningRepasPage.tsx
@@ -43,6 +43,8 @@ export default function PlanningRepasPage() {
                 <Planning/>
 
                 <Edit/>
+
+                <div className={'h-20'}></div>
             </div>
         </PlanningRepasContext.Provider>
     );
