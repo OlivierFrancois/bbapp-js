2363b660e768689f9c2def2801648aec8da5b7a3
ofrancois <olivier.francois09@gmail.com>
2024-05-11 12:09:26 +0200

    ajustement front + fix zoom iphone

--- bbapp-front/src/components/PlanningRepas/Cell.tsx
+++ bbapp-front/src/components/PlanningRepas/Cell.tsx
@@ -21,7 +21,7 @@ export default function Cell({date, moment, planningRepas}: CellProps) {
 
     return <div onClick={handleClick} className={'p-2 h-full flex items-start flex-wrap gap-1 hover:bg-gray-50 cursor-pointer'}>
         {planningRepas?.plats.map((plat) => (
-            <div className={'bg-slate-200 hover:bg-slate-300 cursor-pointer px-2 text-center rounded-full first-letter:uppercase'}>{plat.nom}</div>
+            <div className={'bg-slate-200 hover:bg-slate-300 cursor-pointer px-2 text-center rounded-full first-letter:uppercase text-nowrap overflow-hidden text-ellipsis'}>{plat.nom}</div>
             ))
         }
     </div>
