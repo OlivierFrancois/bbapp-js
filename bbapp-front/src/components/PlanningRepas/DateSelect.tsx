1d63a593c7ac23ce695089601d21eccb3bcf0b53
ofrancois <olivier.francois09@gmail.com>
2024-05-11 11:27:56 +0200

    ajustement front

--- bbapp-front/src/components/PlanningRepas/DateSelect.tsx
+++ bbapp-front/src/components/PlanningRepas/DateSelect.tsx
@@ -10,7 +10,7 @@ export default function DateSelect() {
     }
 
     return (
-        <div className={'bg-gradient-to-br from-primary to-primary/85 from-10% h-12 text-secondary-content flex justify-between items-center p-1 text-2xl shadow'}>
+        <div className={'bg-gradient-to-br from-primary to-primary/85 from-10% h-12 text-secondary-content flex justify-between items-center py-1 px-2 text-2xl shadow'}>
             <div onClick={() => handleDateChange(dayjs(date).add(-1, 'week').format('YYYY-MM-DD'))} className="btn btn-sm">
                 <i className="fa fa-arrow-left"></i>
             </div>
