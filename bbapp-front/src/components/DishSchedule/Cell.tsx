f5b2b6cc47eb64de9417fe24d056c72608c74367
OlivierFrancois <olivier.francois09@gmail.com>
2024-07-09 18:11:55 +0200

    fonts

--- bbapp-front/src/components/DishSchedule/Cell.tsx
+++ bbapp-front/src/components/DishSchedule/Cell.tsx
@@ -17,8 +17,9 @@ export default function Cell({date, moment, dishScheduleItem}: CellProps) {
 
     return <div onClick={handleClick} className={'p-2 h-full flex items-start flex-wrap gap-1 hover:bg-gray-50 cursor-pointer'}>
         {dishScheduleItem?.dishes.map((dish, key) => (
-            <div key={key} className={'bg-slate-200 hover:bg-slate-300 cursor-pointer px-2 text-center rounded-full first-letter:uppercase text-nowrap overflow-hidden text-ellipsis'}>{dish.name}</div>
-            ))
-        }
+            <div key={key} className={'bg-slate-200 hover:bg-slate-300 cursor-pointer px-2 text-center rounded-full first-letter:uppercase text-nowrap overflow-hidden text-ellipsis'}>
+                {dish.name}
+            </div>
+        ))}
     </div>
 }
