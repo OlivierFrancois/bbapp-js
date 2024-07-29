6dc0046831d7007dfffb7d1f2fb8a54b92fc9a98
ofrancois <olivier.francois09@gmail.com>
2024-07-29 17:00:17 +0200

    quick category select when add article from category

--- bbapp-front/src/components/Article/List.tsx
+++ bbapp-front/src/components/Article/List.tsx
@@ -5,6 +5,11 @@ import {ArticleContext} from "../../routes/ArticlePage.tsx";
 export default function List() {
     const {setSelectedArticle, setSelectedCategory,categories, articles} = useContext(ArticleContext);
 
+    const handleArticleCreate = (categoryId: number) => {
+        const article: Article = {id: 0, name: '', sortOrder: 0, categoryId: categoryId};
+        setSelectedArticle(article);
+    }
+
     return <div className={'overflow-auto'}>
         <table className={'table table-sm table-pin-rows'}>
             {categories
@@ -15,9 +20,16 @@ export default function List() {
                             <th className={'text-lg uppercase'}>{category.name}</th>
 
                             <th className={'w-10'}>
-                                <div className="btn btn-primary btn-xs btn-circle"
-                                     onClick={() => setSelectedCategory(category)}>
-                                    <i className={'fa fa-pen'}></i>
+                                <div className="flex items-center justify-center gap-2">
+                                    <button className="btn btn-primary btn-xs btn-circle"
+                                         onClick={() => handleArticleCreate(category.id)}>
+                                        <i className={'fa fa-plus'}></i>
+                                    </button>
+
+                                    <button className="btn btn-primary btn-xs btn-circle"
+                                         onClick={() => setSelectedCategory(category)}>
+                                        <i className={'fa fa-pen'}></i>
+                                    </button>
                                 </div>
                             </th>
                         </tr>
