5aec769e06ccaf4acaf91f38cd917054681a66e3
ofrancois <olivier.francois09@gmail.com>
2024-08-21 16:27:23 +0200

    enable cors

--- bbapp-nest/src/main.ts
+++ bbapp-nest/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
