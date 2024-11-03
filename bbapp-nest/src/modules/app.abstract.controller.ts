35ffa6f16f11930567cfaf3be96517c739ed8b37
OlivierFrancois <olivier.francois09@gmail.com>
2024-11-03 12:39:58 +0100

    add home in abstract controller

--- bbapp-nest/src/modules/app.abstract.controller.ts
+++ bbapp-nest/src/modules/app.abstract.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, UseInterceptors } from '@nestjs/common';
-import { User } from '@prisma/client';
+import { Home, User } from '@prisma/client';
 import { AppSessionInterceptor } from './app-session.interceptor';
 
 @Controller()
 @UseInterceptors(AppSessionInterceptor)
 export abstract class AbstractController {
     public user: User;
+    public home: Home;
 }
