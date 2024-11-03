db6d30cc352f51cfdfd63f358969f88afe26323f
OlivierFrancois <olivier.francois09@gmail.com>
2024-11-03 13:17:57 +0100

    includes home in recipe operations

--- bbapp-nest/src/modules/recipes/controllers/recipe.controller.ts
+++ bbapp-nest/src/modules/recipes/controllers/recipe.controller.ts
@@ -1,36 +1,40 @@
-import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, ForbiddenException, Get, Param, Post, Query } from '@nestjs/common';
 import { RecipeItem } from '@prisma/client';
 import { ApiTags } from '@nestjs/swagger';
 import { RecipeService } from '../services/recipe.service';
 import { CreateRecipeItemDto } from '../dtos/create-recipe-item.dto';
+import { AbstractController } from '../../app.abstract.controller';
 
 @ApiTags('Recipes')
 @Controller('api/recipe')
-export class RecipeController {
-    constructor(private readonly recipeService: RecipeService) {}
+export class RecipeController extends AbstractController {
+    constructor(private readonly recipeService: RecipeService) {
+        super();
+        if (!this.home) throw new ForbiddenException();
+    }
 
     @Get('search/by-dish')
     async getByDish(@Query('dishId') dishId: string): Promise<RecipeItem[]> {
-        return this.recipeService.getByDish(Number(dishId));
+        return this.recipeService.getByDish(this.home.id, Number(dishId));
     }
 
     @Get('search/by-article')
     async getByArticle(@Query('dishId') dishId: string): Promise<RecipeItem[]> {
-        return this.recipeService.getByDish(Number(dishId));
+        return this.recipeService.getByDish(this.home.id, Number(dishId));
     }
 
     @Get(':dishId/:articleId')
     async get(@Param('dishId') dishId: string, @Param('articleId') articleId: string): Promise<RecipeItem | null> {
-        return this.recipeService.get(Number(dishId), Number(articleId));
+        return this.recipeService.get(this.home.id, Number(dishId), Number(articleId));
     }
 
     @Post()
     async create(@Body() data: CreateRecipeItemDto): Promise<RecipeItem> {
-        return this.recipeService.save(data);
+        return this.recipeService.save(this.home.id, data);
     }
 
     @Delete(':dishId/:articleId')
     async delete(@Param('dishId') dishId: string, @Param('articleId') articleId: string): Promise<void> {
-        return this.recipeService.delete(Number(dishId), Number(articleId));
+        return this.recipeService.delete(this.home.id, Number(dishId), Number(articleId));
     }
 }
--- bbapp-nest/src/modules/recipes/services/recipe.service.ts
+++ bbapp-nest/src/modules/recipes/services/recipe.service.ts
@@ -7,52 +7,42 @@ import { CreateRecipeItemDto } from '../dtos/create-recipe-item.dto';
 export class RecipeService {
     constructor(private readonly prisma: PrismaService) {}
 
-    async get(dishId: number, articleId: number) {
+    async get(homeId: number, dishId: number, articleId: number) {
         return this.prisma.recipeItem.findUnique({
             where: {
-                dishId_articleId: {
-                    dishId: dishId,
-                    articleId: articleId,
-                },
+                dishId_articleId: { dishId: dishId, articleId: articleId },
+                homeId,
             },
         });
     }
 
-    async getByDish(dishId: number) {
+    async getByDish(homeId: number, dishId: number) {
         return this.prisma.recipeItem.findMany({
-            where: {
-                dishId: dishId,
-            },
+            where: { dishId, homeId },
         });
     }
 
-    async getByArticle(articleId: number): Promise<RecipeItem[]> {
-        return this.prisma.recipeItem.findMany({
-            where: {
-                articleId: { equals: articleId },
-            },
-        });
+    async getByArticle(homeId: number, articleId: number): Promise<RecipeItem[]> {
+        return this.prisma.recipeItem.findMany({ where: { articleId, homeId } });
     }
 
-    async save(data: CreateRecipeItemDto): Promise<RecipeItem> {
+    async save(homeId: number, data: CreateRecipeItemDto): Promise<RecipeItem> {
         return this.prisma.recipeItem.upsert({
-            where: { dishId_articleId: { dishId: data.dishId, articleId: data.articleId } },
-            create: data,
+            where: { dishId_articleId: { dishId: data.dishId, articleId: data.articleId }, homeId },
+            create: { ...data, homeId },
             update: data,
         });
     }
 
-    async deleteByDish(dishId: number): Promise<void> {
-        await this.prisma.recipeItem.deleteMany({ where: { dishId: dishId } });
+    async deleteByDish(homeId: number, dishId: number): Promise<void> {
+        await this.prisma.recipeItem.deleteMany({ where: { dishId: dishId, homeId } });
     }
 
-    async delete(dishId: number, articleId: number): Promise<void> {
+    async delete(homeId: number, dishId: number, articleId: number): Promise<void> {
         await this.prisma.recipeItem.delete({
             where: {
-                dishId_articleId: {
-                    dishId: dishId,
-                    articleId: articleId,
-                },
+                dishId_articleId: { dishId: dishId, articleId: articleId },
+                homeId,
             },
         });
     }
