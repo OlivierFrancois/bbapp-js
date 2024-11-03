5dda989c77abd2b3877b34dabec2774dcbf7b2ef
OlivierFrancois <olivier.francois09@gmail.com>
2024-11-03 12:19:05 +0100

    check if home is defined

--- bbapp-front/src/pages/home/components/body.component.tsx
+++ bbapp-front/src/pages/home/components/body.component.tsx
@@ -6,9 +6,16 @@ export default function HomeBody() {
     return (
         <div className={'flex-1 py-4 px-5 flex flex-col gap-4'}>
             <div className={'flex-1'}>
-                <h3 className="font-semibold text-xl">
-                    Membres de <span className={'font-bold'}>{home?.name}</span>
-                </h3>
+                {home ? (
+                    <h3 className="font-semibold text-xl">
+                        Membres de <span className={'font-bold'}>{home?.name}</span>
+                    </h3>
+                ) : (
+                    <div className="bg-gray-100 rounded p-2 flex gap-4 items-center text-sm">
+                        <i className="fa fa-warning fa-2x text-warning"></i>
+                        Vous n'appartenez à aucun foyer. Contactez un administrateur.
+                    </div>
+                )}
             </div>
 
             <button onClick={logout} className={'btn btn-outline btn-error'}>
