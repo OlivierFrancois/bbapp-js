e564a95358bbb123e56a78f9f4c5efc5608ea3d3
ofrancois <olivier.francois09@gmail.com>
2024-11-06 08:41:48 +0100

    update dish api

--- bbapp-front/src/lib/api/DishAPI.tsx
+++ bbapp-front/src/lib/api/DishAPI.tsx
@@ -1,25 +1,18 @@
 import { Dish } from '../../types/Dish.tsx';
 import { RecipeItem } from '../../types/RecipeItem.tsx';
 import api from '../api.ts';
-
-const API_HOST = import.meta.env.VITE_API_ENDPOINT;
-const URL_GET_BY_NAME = `${API_HOST}/dish/search/by-name`;
-const URL_GET = `${API_HOST}/dish/:dishId`;
-const URL_UPDATE = `${API_HOST}/dish/:dishId`;
-const URL_DELETE = `${API_HOST}/dish/:dishId`;
-const URL_GET_ALL = `${API_HOST}/dish`;
-const URL_CREATE = `${API_HOST}/dish`;
+import { API_ENDPOINTS } from '../../routes.ts';
 
 export class DishAPI {
-    static async get(id: number): Promise<Dish[]> {
-        return api.get(URL_GET.replace(':dishId', id.toString())).then((res) => {
+    static async get(id: number): Promise<Dish> {
+        return api.get(API_ENDPOINTS.dish.get(id)).then((res) => {
             return res.data;
         });
     }
 
     static async getByName(payload: { name: string }): Promise<Dish[]> {
         return api
-            .get(URL_GET_BY_NAME, {
+            .get(API_ENDPOINTS.dish.getByName, {
                 params: payload,
             })
             .then((res) => {
@@ -28,25 +21,25 @@ export class DishAPI {
     }
 
     static async getAll(): Promise<Dish[]> {
-        return api.get(URL_GET_ALL, {}).then((res) => {
+        return api.get(API_ENDPOINTS.dish.getAll).then((res) => {
             return res.data;
         });
     }
 
     static async create(dish: Omit<Dish, 'id'>): Promise<Dish> {
-        return api.post(URL_CREATE, { ...dish }).then((res) => {
+        return api.post(API_ENDPOINTS.dish.create, { ...dish }).then((res) => {
             return res.data;
         });
     }
 
     static async update(dish: Dish, recipeItems: RecipeItem[]): Promise<{ dish: Dish; recipeItems: RecipeItem[] }> {
-        return api.put(URL_UPDATE.replace(':dishId', dish.id.toString()), { ...dish, recipeItems }).then((res) => {
+        return api.put(API_ENDPOINTS.dish.update(dish.id), { ...dish, recipeItems }).then((res) => {
             return res.data;
         });
     }
 
     static async delete(dishId: number): Promise<{ message: string }> {
-        return api.delete(URL_DELETE.replace(':dishId', dishId.toString())).then((res) => {
+        return api.delete(API_ENDPOINTS.dish.delete(dishId)).then((res) => {
             return res.data;
         });
     }
