364ff4a92816793138dd3fd836f90da9bfef4514
OlivierFrancois <olivier.francois09@gmail.com>
2024-11-07 18:11:21 +0100

    remove old

--- bbapp-front/src/pages/_old/home/home.page.tsx
+++
@@ -1,7 +0,0 @@
-export default function HomePage() {
-    return (
-        <div className={'min-h-screen flex items-center justify-center'}>
-            <div className={'text-xl font-semibold animate-bounce'}>Home</div>
-        </div>
-    );
-}
